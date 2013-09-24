07453c511626f38e38aa217deb5506d0f178a5d3
Crag Wolfe <cwolfe@redhat.com>
2013-09-24 19:02:56 -0400

    wip

--- manifests/node.pp
+++ manifests/node.pp
@@ -4,30 +4,7 @@ class hamysql::node (
   $glance_db_password          = $quickstack::params::glance_db_password,
   $nova_db_password            = $quickstack::params::nova_db_password,
   $cinder_db_password          = $quickstack::params::cinder_db_password,
-
 ) inherits quickstack::params {
-
-    class {'openstack::db::mysql':
-        mysql_root_password  => $mysql_root_password,
-        keystone_db_password => $keystone_db_password,
-        glance_db_password   => $glance_db_password,
-        nova_db_password     => $nova_db_password,
-        cinder_db_password   => $cinder_db_password,
-        neutron_db_password  => '',
-
-        # MySQL
-        mysql_bind_address     => '0.0.0.0',
-        mysql_account_security => true,
-
-        # Cinder
-        cinder                 => false,
-
-        # neutron
-        neutron                => false,
-
-        allowed_hosts          => '%',
-        enabled                => true,
-    }
 
     yumrepo { 'clusterlabs' :
         baseurl => "http://clusterlabs.org/64z.repo",
@@ -35,19 +12,49 @@ class hamysql::node (
         priority => 1,
     }
 
+    package { 'MySQL-python' :
+       ensure => installed,
+    }
+    package { 'mysql-server' :
+       ensure => installed,
+    }
+
+#    class {'openstack::db::mysql':
+#        mysql_root_password  => $mysql_root_password,
+#        keystone_db_password => $keystone_db_password,
+#        glance_db_password   => $glance_db_password,
+#        nova_db_password     => $nova_db_password,
+#        cinder_db_password   => $cinder_db_password,
+#        neutron_db_password  => '',
+#
+#        # MySQL
+#        mysql_bind_address     => '0.0.0.0',
+#        mysql_account_security => true,
+#
+#        # Cinder
+#        cinder                 => false,
+#
+#        # neutron
+#        neutron                => false,
+#
+#        allowed_hosts          => '%',
+#        enabled                => true,
+#    }
+
+
     class {'pacemaker::corosync':
         cluster_name => "hamysql", 
-        cluster_members => "192.168.200.2 192.168.200.3 192.168.200.4 ",
-        require => Class['openstack::db::mysql'], Yumrepo['clusterlabs'],
+        cluster_members => "192.168.200.11 192.168.200.12 192.168.200.13 ",
+        require => [Yumrepo['clusterlabs'],Package['mysql-server'],Package['MySQL-python']],
     }
 
     class {"pacemaker::resource::ip":
-      ip_address => "192.168.200.10",
+      ip_address => "192.168.200.50",
       group => "my_group",
     }
 
     class {"pacemaker::resource::filesystem":
-       device => "192.168.200.255:/var/www/html",
+       device => "192.168.200.200:/mnt/mysql",
        directory => "/var/lib/mysql",
        fstype => "nfs",
     }
